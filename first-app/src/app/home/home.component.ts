9930db26ef3611638bf0c8bee6d56fedf1f8a809
Yasunori-aloha <gyarubaku@yahoo.ne.jp>
2024-06-04 03:54:53 +0000

    feat: 検索窓を追加。

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -4,7 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {

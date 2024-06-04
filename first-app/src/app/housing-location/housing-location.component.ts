13212a54d0ff91fdf8f3c8afa150a3ab74b24426
Yasunori-aloha <gyarubaku@yahoo.ne.jp>
2024-06-04 06:43:42 +0000

    feat: ロケーションの表示要素を追加。

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -5,7 +5,20 @@ import { HousingLocation } from '../housing-location';
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
-  templateUrl: './housing-location.component.html',
+  template: `
+    <section class="listing">
+      <img
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        class="listing-photo"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
   styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {

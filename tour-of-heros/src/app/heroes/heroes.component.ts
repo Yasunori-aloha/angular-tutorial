c265c19aabd9d9f9343fceb394ce8b22b9d57fdf
Yasunori-aloha <gyarubaku@yahoo.ne.jp>
2024-06-07 10:17:27 +0000

    feat: ヒーロー詳細の表示要素を別コンポーネントに分離。

--- tour-of-heros/src/app/heroes/heroes.component.ts
+++ tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, CommonModule],
+  imports: [CommonModule, UpperCasePipe, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })

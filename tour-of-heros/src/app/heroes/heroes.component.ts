a00a1b7205951ba03c448bbe7f473bfa4fc95263
Yasunori-aloha <gyarubaku@yahoo.ne.jp>
2024-06-07 10:43:00 +0000

    feat: データ取得時などに表示するメッセージサービスを追加。

--- tour-of-heros/src/app/heroes/heroes.component.ts
+++ tour-of-heros/src/app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@ import { Hero } from '../hero';
 import { CommonModule, UpperCasePipe } from '@angular/common';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,13 +16,21 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }

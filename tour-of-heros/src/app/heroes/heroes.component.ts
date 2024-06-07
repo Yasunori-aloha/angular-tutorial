3c649fd2c6494b73c1cd2b6f9ff4bab083aca23d
Yasunori-aloha <gyarubaku@yahoo.ne.jp>
2024-06-07 10:27:46 +0000

    feat: ヒーロー一覧の取得処理をサービスに分離。

--- tour-of-heros/src/app/heroes/heroes.component.ts
+++ tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule, UpperCasePipe } from '@angular/common';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,10 +11,15 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
-export class HeroesComponent {
-  heroes = HEROES;
-
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
